9ec5cf0d51e631b0dc7af918b3edb352f9290254
Melekhov Andrey <cool.melekhov@gmail.com>
2024-04-19 01:12:23 +0300

    application properties

--- src/test/java/com/example/runningclubs/RunningClubsApplicationTests.java
+++ src/test/java/com/example/runningclubs/RunningClubsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RunningClubsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
