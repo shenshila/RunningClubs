a6106bd7f3b7f5912303fb76a46dd5960109f179
Melekhov Andrey <cool.melekhov@gmail.com>
2024-04-19 00:25:04 +0300

    Initial commit

---
+++ src/main/java/com/example/runningclubs/RunningClubsApplication.java
@@ -0,0 +1,13 @@
+package com.example.runningclubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningClubsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningClubsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/runningclubs/RunningClubsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.runningclubs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningClubsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
